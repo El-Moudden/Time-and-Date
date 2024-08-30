96bd3d43fd2e7891ed6058b3d0802b0d732c0af2
El Moudden Abdelhalim <117116526+El-Moudden@users.noreply.github.com>
2024-08-30 08:03:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let clock = document.querySelector(".clock");
+let hours = document.querySelector(".hours");
+let minutes = document.querySelector(".minutes");
+let seconds = document.querySelector(".seconds");
+let date = document.querySelector(".date");
+
+function timeNow(){
+    let nowClock = new Date()
+    hours.innerHTML = nowClock.getHours().toString().padStart(2,"0") + ": ";
+    minutes.innerHTML = nowClock.getMinutes().toString().padStart(2,"0") + ": ";
+    seconds.innerHTML = nowClock.getSeconds().toString().padStart(2,"0") ;
+    let days = ["MONDAY","TUESDAY","WEDSDAY","THURSDAY","FRIDAY","SATURDAY","SUNDAY"];
+    let months = ["JANUARY","FEBRUARY","MARCH","APRIL","MAY","JUNE","JULY","AUGUST","SEPTEMBER","OCTOBER","NOVEMBER","DECEMBER"];
+   
+    date.innerHTML = `
+        <span class="day">${days[nowClock.getDay()]}</span>
+        <span class="today">${nowClock.getDate()}</span>
+        <span class="month">${months[nowClock.getMonth()].toString().padStart(2,"0")}</span>
+    `
+
+    
+}
+
+
+
+
+
+setInterval(timeNow, 1000);
